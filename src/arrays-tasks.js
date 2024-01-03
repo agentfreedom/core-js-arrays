3d674f58a28adfb93425f7b7bae5c8c82356e153
Avdasov Oleksandr <agentfreedom@zoho.com>
2024-01-04 01:52:15 +0200

    feat: add first solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,14 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  // throw new Error('Not implemented');
+  // const arr = [];
+  // for (let i = start; i <= end; i += 1) {
+  //   arr.push(i);
+  // }
+  // return arr;
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i);
 }
 
 /**
