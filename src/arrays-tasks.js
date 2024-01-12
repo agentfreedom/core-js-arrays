be522fb5704cce8ae6010ecbf45e0238b4ab9dfc
Avdasov Oleksandr <agentfreedom@zoho.com>
2024-01-12 02:52:02 +0200

    feat: add solution getTail function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -232,8 +232,9 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  // throw new Error('Not implemented');
+  return arr.slice(arr.length - n);
 }
 
 /**
