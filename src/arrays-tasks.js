54db3662b61fb12c0b269fbe49de2f9c1ac1bc26
Avdasov Oleksandr <agentfreedom@zoho.com>
2024-01-12 03:05:25 +0200

    feat: add solution createNDimensionalArray function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -300,8 +300,15 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  // throw new Error('Not implemented');
+  if (n < 1) {
+    return 0;
+  }
+
+  return Array.from({ length: size }).fill(
+    createNDimensionalArray(n - 1, size)
+  );
 }
 
 /**
